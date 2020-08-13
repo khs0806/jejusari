090028dc9437841865ff5b39de290f6b11cb3306
2ket <hello2ket@gmail.com>
2020-08-13 18:13:34 +0900

    이미지 업로드 적용중

--- src/main/java/com/kitri/jejusari/controller/SalesController.java
+++ src/main/java/com/kitri/jejusari/controller/SalesController.java
@@ -1,13 +1,25 @@
 package com.kitri.jejusari.controller;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.apache.commons.io.FileUtils;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.kitri.jejusari.dto.SalesDto;
@@ -77,5 +89,33 @@ public class SalesController {
 
 //		return mav;
 	}
-
+	
+	
+	/** 이미지 관련 controller 함수..! 작성중입니다.(kke) */
+	@RequestMapping(value="/uploadSummernoteImageFile", method=RequestMethod.POST)
+	@ResponseBody
+	public JSONObject uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile) {
+		JSONObject obj=new JSONObject();
+		
+		String fileRoot="C:\\jejusari\\summernote_img\\";		//저장될 외부 파일 경로
+		//없는 경로면 생성하는 코드 만들어야하지 않나??
+		String originalFileName=multipartFile.getOriginalFilename();	//오리지날 파일명
+		String extention = originalFileName.substring(originalFileName.lastIndexOf("."));	//확장자명
+		
+		String savedFileName=UUID.randomUUID()+extention;		//저장될 파일 명
+		File targetFile=new File(fileRoot+savedFileName);
+		
+		try {
+			InputStream fileStream=multipartFile.getInputStream();
+			FileUtils.copyInputStreamToFile(fileStream, targetFile);	//파일저장
+			obj.put("url", "/jejusari/summernote_img/"+savedFileName);
+			obj.put("responseCode", "success");
+			
+		}catch(IOException e) {
+			FileUtils.deleteQuietly(targetFile);	//실패시 저장된 파일 삭제
+			obj.put("responseCode", "error");
+			e.printStackTrace();
+		}
+		return obj;
+	}
 }
